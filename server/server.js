980005e2ef50330f3d680e647df42be1ba02f843
Jonas Duemmler <jk.duemmler@gmail.com>
2018-10-17 13:44:23 +0200

    Refurbished initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Server',
+    text: 'Hey. What is going on.',
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('New message:', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
